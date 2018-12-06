4b2de7acf143cd94af72d909cd114feefd9c856e
Daniel Kentfield <dan@mymac.com>
2018-12-06 18:25:28 +0300

    First commit

---
+++ src/components/nav-bar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles.css'
+
+export default (props) => {
+  return(
+      <div className='navbar'>
+          <h2 >App Bar</h2>
+          
+      </div>
+  )
+}
